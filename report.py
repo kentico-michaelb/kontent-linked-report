f4b1e05d72be4af32da465c390215736f474eabe
kentico-michaelb <michaelb@kentico.com>
2021-05-18 11:36:56 -0400

    Adds responsive design and zoom reset

--- report.py
+++ report.py
@@ -19,7 +19,7 @@ app.index_string = '''
         {%css%}
     </head>
     <body style="background-color: #918F8F">
-        <div style="background-color:black">
+        <div style="background-color:black;">
         <img id="logo" 
                 alt="Kontent" 
                 src="static\\images\\02-kk-logo-col-blk.svg"
@@ -36,25 +36,56 @@ app.index_string = '''
 </html>
 '''
 
-app.layout = html.Div([
-    html.Div([
+styles = {
+    "container": {
+        "position": "fixed",
+        "display": "flex",
+        "flexDirection": "column",
+        "flexWrap": "wrap",
+        "alignContent": "flex-start",
+        "height": "100%",
+        "width": "100%"
+    },
+    "cy-container": {
+        "position": "relative",
+        "backgroundColor":"#E9E8E8",
+        "width": "74%",
+        "height": "100%",
+        "margin": "auto"
+    },
+    "cytoscape": {
+        "position": "absolute",
+        "width": "100%",
+        "height": "100%",
+        "zIndex": 999
+    },
+    "info": {
+        "width": "25%",
+        "height": "40%",
+        "position": "relative",
+        "backgroundColor": "#D3D2D2",
+        "padding": "10px"
+    },
+    "btn-reset": {
+        "marginBottom":"20px"
+    }
+}
+
+app.layout = html.Div(style=styles["container"], children=[
+    html.Div(style=styles["info"], children=[
         html.H1("Konstellation for project:"),
         html.H2(f"{config.project_id}"),
         html.P("Hover over links to view linked item relationships:"),
-        html.Span(id='cytoscape-mouseoverEdgeData-output'),
-    ],
-    style={
-        "display": "inline-block",
-        "fontFamily": "Arial",
-        "width": "30%",
-        "height": "300px",
-        "verticalAlign": "top",
-        "backgroundColor":" #D3D2D2"}
-    ),
-    html.Div([
+        html.Button("Reset Zoom", id="btn-reset", style=styles["btn-reset"]),
+        html.Div(id="cytoscape-mouseoverEdgeData-output"),
+    ]),
+    html.Div(className="cy-container", style=styles["cy-container"], children=[
         cyto.Cytoscape(
             id="linked_items_report",
             elements=nodes,
+            style=styles["cytoscape"],
+            zoomingEnabled=True,
+            zoom=2,
             layout={
                 "name": "cose",
                 "idealEdgeLength": 100,
@@ -68,10 +99,7 @@ app.layout = html.Div([
                 "edgeElasticity": 100,
                 "nestingFactor": 5
             },
-            style={"width": "800px", 
-                "height": "800px",
-                "backgroundColor": "#E9E8E8",
-                "margin":"auto"},
+            responsive=True,
             stylesheet=[
             {
                 "selector": "node",
@@ -97,16 +125,24 @@ app.layout = html.Div([
             }
         ]
         ),
-    ],
-    style={
-    "display": "inline-block",
-    }),   
+    ]),       
 ])
 
-@app.callback(Output('cytoscape-mouseoverEdgeData-output', 'children'),
-                Input('linked_items_report', 'mouseoverEdgeData'))
+
+@app.callback(Output("cytoscape-mouseoverEdgeData-output", "children"),
+                Input("linked_items_report", "mouseoverEdgeData"))
 def displayTapEdgeData(data):
     if data:
         return f"\"{data['target']}\" linked to \"{data['source']}\" by the \"{data['element']}\" element."
+
+
+@app.callback(
+    [Output("linked_items_report", "zoom"),
+     Output("linked_items_report", "elements")],
+    [Input("btn-reset", "n_clicks")]
+)
+def reset_layout(n_clicks):
+    return [1, nodes]
+
 
 app.run_server(debug=True)
